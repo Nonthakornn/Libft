d435fb5f77b85eb86da13818631fc1213cbc8150
cinn4m0n <98396873+WCM-1310@users.noreply.github.com>
2024-08-28 16:36:14 +0700

    Add files via upload

---
+++ ft_strdup.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+#include <stdlib.h>
+
+char *ft_strdup(const char *s)
+{
+    char *duplicate;
+    int i;
+
+    duplicate = (char *) malloc(ft_strlen(s) + 1);
+    i = 0;
+
+    while (s[i])
+    {
+        duplicate[i] = s[i];
+        i++;
+    }
+    duplicate[i] = 0;
+
+    return (duplicate);
+}
+
+// int main(void)
+// {
+//     char *ptr;
+//     printf("\nTest de ft_strdup :\n");
+// 	ptr = ft_strdup("matcha latte");
+// 	printf("'matcha latte' : %s\n", ptr);
+// 	free(ptr);
+// }
