e73cddf25915db56b41a7e319ee7ff18da1fff8e
Warisa Charoencharasratsamee <warisac@Warisas-MacBook-Pro.local>
2024-08-29 15:52:10 +0700

    ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+#include <stdlib.h>
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char *substring;
+    size_t i;
+
+    substring = (char *) malloc(len + 1);
+    i = 0;
+
+    if (len < 0 || start < 0)
+        return NULL;
+
+    while (s[start] != '\0' && len > 0)
+    {
+        substring[i] = s[start];
+        len--;
+        i++;
+        start++;
+    }
+    substring[i] = '\0';
+
+    return substring;
+}
+
+int main(void)
+{
+    printf("\nTest de ft_substr :\n");
+	printf("njo : %s\n", ft_substr("bonjour", 2, 3));
+	printf("njour : %s\n", ft_substr("bonjour", 2, 5));
+	printf("njour : %s\n", ft_substr("bonjour", 2, 6));
+	printf("r : %s\n", ft_substr("bonjour", 6, 1));
+	printf("'' : %s\n",	ft_substr("bonjour", 6, 0));
+}
