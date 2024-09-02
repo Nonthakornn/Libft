64075d6e9a98575d7f6324a5d339da7dc5e290ea
Warisa Charoencharasratsamee <warisac@Warisas-MacBook-Pro.local>
2024-09-02 12:50:33 +0700

    ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,7 +1,30 @@
 #include "libft.h"
+#include <stdlib.h>
+#include "ft_strlen.c"
 
-/*Allocates (with malloc()) and returns a new string, which is the result of the concatenation of s1 & s2*/
 char *ft_strjoin(char const *s1, char const *s2)
 {
+    char *new_string;
+    char *ptr;
 
+    new_string = (char *) malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+    ptr = new_string;
+
+    while (*s1)
+        *ptr++ = *s1++;
+    while (*s2)
+        *ptr++ = *s2++;
+    
+    *ptr = '\0';
+
+    return new_string;
+}
+
+int main(void)
+{
+    printf("\nTest de ft_strjoin :\n");
+	printf("abcdef : %s\n",	ft_strjoin("abc", "def"));
+	printf("abc : %s\n", ft_strjoin("", "abc"));
+	printf("abc : %s\n", ft_strjoin("abc", ""));
+	printf("'' : %s\n", ft_strjoin("", ""));
 }
