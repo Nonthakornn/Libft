7ea6d8ef2c27dae7dccf84ab3cf6fcd7777a0422
Warisa Charoencharasratsamee <warisac@Warisas-MacBook-Pro.local>
2024-09-04 11:41:09 +0700

    update

--- ft_substr.c
+++ ft_substr.c
@@ -6,10 +6,10 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
     char *substring;
     size_t i;
 
-    substring = (char *) malloc(len + 1);
+    substring = (char *) malloc((len + 1) * sizeof(char));
     i = 0;
 
-    if (len < 0 || start < 0)
+    if (!s)
         return NULL;
 
     while (s[start] != '\0' && len > 0)
@@ -24,12 +24,12 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
     return substring;
 }
 
-int main(void)
-{
-    printf("\nTest de ft_substr :\n");
-	printf("njo : %s\n", ft_substr("bonjour", 2, 3));
-	printf("njour : %s\n", ft_substr("bonjour", 2, 5));
-	printf("njour : %s\n", ft_substr("bonjour", 2, 6));
-	printf("r : %s\n", ft_substr("bonjour", 6, 1));
-	printf("'' : %s\n",	ft_substr("bonjour", 6, 0));
-}
+// int main(void)
+// {
+//     printf("\nTest de ft_substr :\n");
+// 	printf("njo : %s\n", ft_substr("bonjour", 2, 3));
+// 	printf("njour : %s\n", ft_substr("bonjour", 2, 5));
+// 	printf("njour : %s\n", ft_substr("bonjour", 2, 6));
+// 	printf("r : %s\n", ft_substr("bonjour", 6, 1));
+// 	printf("'' : %s\n",	ft_substr("bonjour", 6, 0));
+// }
--- libft.h
+++ libft.h
@@ -22,6 +22,7 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
 char *ft_strnstr(const char *haystack, const char *needle, size_t len);
+char *ft_substr(char const *s, unsigned int start, size_t len);
 
 size_t  ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
