13fbbb9a9dd9d4f6c9b8695279d0047126de1118
Nonthakorn <nonthakorn.che@gmail.com>
2024-10-06 14:20:07 +0700

    update ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nchencha <nchencha@student.42.fr>          #+#  +:+       +#+        */
+/*   By: nchencha <nchencha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-09-04 11:12:08 by nchencha          #+#    #+#             */
-/*   Updated: 2024-09-04 11:12:08 by nchencha         ###   ########.fr       */
+/*   Created: 2024/09/04 11:12:08 by nchencha          #+#    #+#             */
+/*   Updated: 2024/10/06 14:18:33 by nchencha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *str)
 	size_t	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 	{
 		i++;
